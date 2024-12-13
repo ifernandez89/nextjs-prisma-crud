34f32d043dc88ff13e4ff8131cf3481d43c3c7b7
igfernandez <igfernandez@parana.gob.ar>
2024-12-13 13:33:30 -0300

    comentando la interface Params que se definia pero nunca se usaba

--- src/app/api/task/[id]/route.ts
+++ src/app/api/task/[id]/route.ts
@@ -1,20 +1,6 @@
 import { NextResponse } from "next/server";
 import { prisma } from "@/libs/prisma";
 
-interface Params {
-  params: { id: string };
-}
-
-/*export async function GET(request: Request, { params }: { params: { id: string } }) {
-  const task = await prisma.task.findFirst({
-    where: {
-      id: Number(params.id),
-    },
-  });
-
-  return NextResponse.json(task);
-}*/
-
 export async function GET(request: Request, { params }: { params: { id: string } }) {
   try {
     // Convert the 'id' from string to number
