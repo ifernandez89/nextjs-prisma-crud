cce820a855dd4ee97183cb1fd832951ceda4bb66
igfernandez <igfernandez@parana.gob.ar>
2024-12-16 13:16:06 -0300

    cambiando unos colores

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,12 +8,12 @@ function Navbar() {
       <ul className="flex gap-4 justify-center">
         <li>
           <Link href="/" className="text-slate-600 hover:text-slate-400 text-xl">
-          <h3 className="text-2xl">Next CRUD - Welcome to the homepage!</h3>
+          <h3 className="text-2xl bg-gray-800 text-slate-600">Next CRUD - Welcome to the homepage!</h3>
           </Link>
         </li>
         <li>
           <Link href="/new" className="text-slate-600 hover:text-slate-400 text-xl">
-          <h3 className="text-2xl"> Nueva Tarea</h3>
+          <h3 className="text-2xl bg-gray-800 text-slate-600"> Nueva Tarea</h3>
           </Link>
         </li>
       </ul>
