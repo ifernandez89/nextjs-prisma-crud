260083929d500a381542dd6d13dae8c44e89e3e9
igfernandez <igfernandez@parana.gob.ar>
2024-12-16 08:08:13 -0300

    arreglando el otro route.ts, quitando params explicito

--- src/app/api/task/route.ts
+++ src/app/api/task/route.ts
@@ -1,10 +1,12 @@
-import { NextResponse } from "next/server";
+import { NextResponse, NextRequest } from "next/server";
 import {prisma} from '@/libs/prisma'
 
-export async function GET(request: Request, { params }: { params: { id: string } }){
-    try {
+export async function GET(req: NextRequest) {//export async function GET(request: Request, { params }: { params: { id: string } }){
+  const url = req.nextUrl; // Extrae el objeto URL
+  const id = url.pathname.split('/').pop(); // Obtiene el valor de 'id' desde la ruta  
+  try {
         // Convert the id to a number before passing it to Prisma
-        const taskId = parseInt(params.id, 10); // Convert string to number
+        const taskId = Number(id); // Convert string to number
     
         if (isNaN(taskId)) {
           return NextResponse.json({ message: 'Invalid ID' }, { status: 400 });
